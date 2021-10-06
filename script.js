c9d0f993febe40a30c9f68efd4f85df88f001284
Greg Testo <gregt@pursuit.org>
2021-10-06 12:51:25 -0400

    added detail specs and added am add to teams button

--- script.js
+++ script.js
@@ -32,6 +32,20 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
                 .then((res)=>res.json())
                 .then((data)=>{
                     console.log(data);
+                    let hp;
+                    let atk;
+                    let def;
+                    
+                    for(let statObj of data.stats){
+                        if(statObj.stat.name === "hp"){
+                            hp = statObj.base_stat;
+                        } else if(statObj.stat.name === "attack"){
+                            atk = statObj.base_stat;
+                        } else if(statObj.stat.name === "defense"){
+                            def = statObj.base_stat;
+                        }
+                    }
+
                     let details = document.querySelector("#details");
 
                     let typeStr = data.types.map((typeEl)=>{
@@ -47,16 +61,35 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
                         <div id="details-text">
                             <div id="details-name">Name: ${data.name}</div>
                             <div id="details-type">Type: ${typeStr}</div>
+                            <div id="details-weight">Weight: ${data.weight} hectograms</div>
+                            <div id="details-height">Height: ${data.height} decimeters</div>
                         </div>
                         <div id="details-sub-text">
                             <h3>Base Attributes</h3>
-                            <div>Hit Points: 35</div>
-                            <div>Attack: 55</div>
-                            <div>Defense: 40</div>
-                        </div>
-                        <div id="details-action">
-                            <button>Add -></button>
+                            <div>Hit Points: ${hp}</div>
+                            <div>Attack: ${atk}</div>
+                            <div>Defense: ${def}</div>
                         </div>`;
+
+                        let recentList = document.querySelector("#recent-list");
+                        // <div class="recent-list-item">
+                        //     <img src="https://static.pokemonpets.com/images/monsters-images-800-800/147-Dratini.webp" alt="Evolution version image" />
+                        //     <div>Dratini</div>
+                        // </div>
+                        let recentListItem = document.createElement("div");
+                        recentListItem.classList.add("recent-list-item");
+
+                        let recentListImg = document.createElement("img");
+                        recentListImg.src = data.sprites.front_default;
+                        recentListImg.alt = "Evolution version image";
+
+                        let nameDiv = document.createElement("div");
+                        nameDiv.textContent = data.name;
+
+                        recentListItem.append(recentListImg, nameDiv);
+
+                        recentList.append(recentListItem);
+
                     // let currentPokemonEl = document.querySelector("#current-pokemon");
 
                     // let img = document.createElement("img");
@@ -70,3 +103,11 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
 
 //         // console.log(selectedPokemon);
     });
+
+let addToTeamButton = document.querySelector("#add-to-team");
+
+addToTeamButton.addEventListener("click", ()=>{
+    let currentPokemonName = document.querySelector("#details-name").textContent;
+    console.log(currentPokemonName);
+    // let currentPokemonImg;
+})
