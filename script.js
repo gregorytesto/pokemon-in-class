4c80cd6be99816409f1e22bf06348279b0e74305
Greg Testo <gregt@pursuit.org>
2021-10-06 11:04:22 -0400

    added single pokemon fetch back in

--- script.js
+++ script.js
@@ -23,49 +23,50 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
 
     form.addEventListener("submit", (e)=>{
         e.preventDefault();
-        let details = document.querySelector("#details");
-        let url = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png";
-        details.innerHTML = `<div id="details-title">
-                <h2>Details</h2>
-            </div>
-            <div id="details-img-container">
-                <img src=${url} alt="Image of selected pokémon" />
-            </div>
-            <div id="details-text">
-                <div id="details-name">Name: Squirtle</div>
-                <div id="details-species">Species: Pikachu</div>
-                <div id="details-type">Type: Electric</div>
-            </div>
-            <div id="details-sub-text">
-                <h3>Base Attributes</h3>
-                <div>Hit Points: 35</div>
-                <div>Attack: 55</div>
-                <div>Defense: 40</div>
-            </div>
-            <div id="details-action">
-                <button>Add -></button>
-            </div>`;
-
-
-//         let selectedPokemon = e.target["pokemon-select"].value;
-
+        let selectedPokemon = e.target["pokemon-select"].value;
 //         // console.log("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
-//         let errMessage = document.querySelector("#error-message");
-//         if( selectedPokemon !== "default" ){
-//             errMessage.textContent = "";
-//             fetch("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
-//                 .then((res)=>res.json())
-//                 .then((data)=>{
-//                     let currentPokemonEl = document.querySelector("#current-pokemon");
+        let errMessage = document.querySelector("#error-message");
+        if( selectedPokemon !== "default" ){
+            errMessage.textContent = "";
+            fetch("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
+                .then((res)=>res.json())
+                .then((data)=>{
+                    console.log(data);
+                    let details = document.querySelector("#details");
 
-//                     let img = document.createElement("img");
-//                     img.src = data.sprites["front_default"];
+                    let typeStr = data.types.map((typeEl)=>{
+                        return typeEl.type.name;
+                    }).join("/");
+
+                    details.innerHTML = `<div id="details-title">
+                            <h2>Details</h2>
+                        </div>
+                        <div id="details-img-container">
+                            <img src=${data.sprites.front_default} alt="Image of selected pokémon" />
+                        </div>
+                        <div id="details-text">
+                            <div id="details-name">Name: ${data.name}</div>
+                            <div id="details-type">Type: ${typeStr}</div>
+                        </div>
+                        <div id="details-sub-text">
+                            <h3>Base Attributes</h3>
+                            <div>Hit Points: 35</div>
+                            <div>Attack: 55</div>
+                            <div>Defense: 40</div>
+                        </div>
+                        <div id="details-action">
+                            <button>Add -></button>
+                        </div>`;
+                    // let currentPokemonEl = document.querySelector("#current-pokemon");
+
+                    // let img = document.createElement("img");
+                    // img.src = data.sprites["front_default"];
                     
-//                     currentPokemonEl.append(img);
-//                 });
-//         } else {
-//             errMessage.textContent = "Please select a Pokemon!";
-//         }
+                    // currentPokemonEl.append(img);
+                });
+        } else {
+            errMessage.textContent = "Please choose a Pokemon!";
+        }
 
 //         // console.log(selectedPokemon);
     });
