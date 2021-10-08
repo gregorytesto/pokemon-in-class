bf245647d50b33fd6a1dae55f4ce5ef4ca9af158
Greg Testo <gregt@pursuit.org>
2021-10-08 10:55:55 -0400

    added click recent item feature

--- script.js
+++ script.js
@@ -1,3 +1,89 @@
+function fetchPokemonDetails(pokemonName, shouldAddToRecent){
+
+    let errMessage = document.querySelector("#error-message");
+    if( pokemonName !== "default" ){
+        errMessage.textContent = "";
+        fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
+            .then((res)=>res.json())
+            .then((data)=>{
+                console.log(data);
+                let hp;
+                let atk;
+                let def;
+                
+                for(let statObj of data.stats){
+                    if(statObj.stat.name === "hp"){
+                        hp = statObj.base_stat;
+                    } else if(statObj.stat.name === "attack"){
+                        atk = statObj.base_stat;
+                    } else if(statObj.stat.name === "defense"){
+                        def = statObj.base_stat;
+                    }
+                }
+
+                let details = document.querySelector("#details");
+
+                let typeStr = data.types.map((typeEl)=>{
+                    return typeEl.type.name;
+                }).join("/");
+
+                details.innerHTML = `<div id="details-title">
+                        <h2>Details</h2>
+                    </div>
+                    <div id="details-img-container">
+                        <img src=${data.sprites.front_default} alt="Image of selected pokémon" />
+                    </div>
+                    <div id="details-text">
+                        <div id="details-name">Name: ${data.name}</div>
+                        <div id="details-type">Type: ${typeStr}</div>
+                        <div id="details-weight">Weight: ${data.weight} hectograms</div>
+                        <div id="details-height">Height: ${data.height} decimeters</div>
+                    </div>
+                    <div id="details-sub-text">
+                        <h3>Base Attributes</h3>
+                        <div>Hit Points: ${hp}</div>
+                        <div>Attack: ${atk}</div>
+                        <div>Defense: ${def}</div>
+                    </div>`;
+
+                    
+                    if(shouldAddToRecent){
+                        let recentList = document.querySelector("#recent-list");
+                        // <div class="recent-list-item">
+                        //     <img src="https://static.pokemonpets.com/images/monsters-images-800-800/147-Dratini.webp" alt="Evolution version image" />
+                        //     <div>Dratini</div>
+                        // </div>
+                        let recentListItem = document.createElement("div");
+                        recentListItem.classList.add("recent-list-item");
+    
+                        let recentListImg = document.createElement("img");
+                        recentListImg.src = data.sprites.front_default;
+                        recentListImg.alt = "Evolution version image";
+    
+                        let nameDiv = document.createElement("div");
+                        nameDiv.textContent = data.name;
+    
+                        nameDiv.addEventListener("click", (event)=>{
+                            fetchPokemonDetails(event.target.textContent, false);
+                        });
+    
+                        recentListItem.append(recentListImg, nameDiv);
+    
+                        recentList.append(recentListItem);
+                    }
+
+                // let currentPokemonEl = document.querySelector("#current-pokemon");
+
+                // let img = document.createElement("img");
+                // img.src = data.sprites["front_default"];
+                
+                // currentPokemonEl.append(img);
+            });
+    } else {
+        errMessage.textContent = "Please choose a Pokemon!";
+    }
+}
+
 fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
     .then((res)=>{
         return res.json();
@@ -25,81 +111,7 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
         e.preventDefault();
         let selectedPokemon = e.target["pokemon-select"].value;
 //         // console.log("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
-        let errMessage = document.querySelector("#error-message");
-        if( selectedPokemon !== "default" ){
-            errMessage.textContent = "";
-            fetch("https://pokeapi.co/api/v2/pokemon/" + selectedPokemon)
-                .then((res)=>res.json())
-                .then((data)=>{
-                    console.log(data);
-                    let hp;
-                    let atk;
-                    let def;
-                    
-                    for(let statObj of data.stats){
-                        if(statObj.stat.name === "hp"){
-                            hp = statObj.base_stat;
-                        } else if(statObj.stat.name === "attack"){
-                            atk = statObj.base_stat;
-                        } else if(statObj.stat.name === "defense"){
-                            def = statObj.base_stat;
-                        }
-                    }
-
-                    let details = document.querySelector("#details");
-
-                    let typeStr = data.types.map((typeEl)=>{
-                        return typeEl.type.name;
-                    }).join("/");
-
-                    details.innerHTML = `<div id="details-title">
-                            <h2>Details</h2>
-                        </div>
-                        <div id="details-img-container">
-                            <img src=${data.sprites.front_default} alt="Image of selected pokémon" />
-                        </div>
-                        <div id="details-text">
-                            <div id="details-name">Name: ${data.name}</div>
-                            <div id="details-type">Type: ${typeStr}</div>
-                            <div id="details-weight">Weight: ${data.weight} hectograms</div>
-                            <div id="details-height">Height: ${data.height} decimeters</div>
-                        </div>
-                        <div id="details-sub-text">
-                            <h3>Base Attributes</h3>
-                            <div>Hit Points: ${hp}</div>
-                            <div>Attack: ${atk}</div>
-                            <div>Defense: ${def}</div>
-                        </div>`;
-
-                        let recentList = document.querySelector("#recent-list");
-                        // <div class="recent-list-item">
-                        //     <img src="https://static.pokemonpets.com/images/monsters-images-800-800/147-Dratini.webp" alt="Evolution version image" />
-                        //     <div>Dratini</div>
-                        // </div>
-                        let recentListItem = document.createElement("div");
-                        recentListItem.classList.add("recent-list-item");
-
-                        let recentListImg = document.createElement("img");
-                        recentListImg.src = data.sprites.front_default;
-                        recentListImg.alt = "Evolution version image";
-
-                        let nameDiv = document.createElement("div");
-                        nameDiv.textContent = data.name;
-
-                        recentListItem.append(recentListImg, nameDiv);
-
-                        recentList.append(recentListItem);
-
-                    // let currentPokemonEl = document.querySelector("#current-pokemon");
-
-                    // let img = document.createElement("img");
-                    // img.src = data.sprites["front_default"];
-                    
-                    // currentPokemonEl.append(img);
-                });
-        } else {
-            errMessage.textContent = "Please choose a Pokemon!";
-        }
+        fetchPokemonDetails(selectedPokemon, true);
 
 //         // console.log(selectedPokemon);
     });
