7fad9039a77f1f8f81066bb87642c7049ff947f5
Greg Testo <gregt@pursuit.org>
2021-10-18 13:06:16 -0400

    Refactored fetch to use async/await

--- script.js
+++ script.js
@@ -2,87 +2,83 @@ function capitalize(str){
     return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
 
-function fetchPokemonDetails(pokemonName, shouldAddToRecent){
+async function fetchPokemonDetails(pokemonName, shouldAddToRecent){
 
     let errMessage = document.querySelector("#error-message");
     if( pokemonName !== "default" ){
         errMessage.textContent = "";
-        fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
-            .then((res)=>res.json())
-            .then((data)=>{
-                console.log(data);
-                let hp;
-                let atk;
-                let def;
-                
-                for(let statObj of data.stats){
-                    if(statObj.stat.name === "hp"){
-                        hp = statObj.base_stat;
-                    } else if(statObj.stat.name === "attack"){
-                        atk = statObj.base_stat;
-                    } else if(statObj.stat.name === "defense"){
-                        def = statObj.base_stat;
-                    }
-                }
 
-                let details = document.querySelector("#details");
+        let data;
+        try{
+            let res = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName.toLowerCase());
+            data = await res.json();    
+        }catch(err){
+            console.log(err);
+        }
 
-                let typeStr = data.types.map((typeEl)=>{
-                    return capitalize(typeEl.type.name);
-                }).join("/");
+        let hp;
+        let atk;
+        let def;
+        
+        for(let statObj of data.stats){
+            if(statObj.stat.name === "hp"){
+                hp = statObj.base_stat;
+            } else if(statObj.stat.name === "attack"){
+                atk = statObj.base_stat;
+            } else if(statObj.stat.name === "defense"){
+                def = statObj.base_stat;
+            }
+        }
 
-                details.innerHTML = `<div id="details-title">
-                        <h2>Details</h2>
-                    </div>
-                    <div id="details-img-container">
-                        <img src=${data.sprites.front_default} alt="Image of selected pokémon" />
-                    </div>
-                    <div id="details-text">
-                        <div>Name: <span id="details-name">${capitalize(data.name)}</span></div>
-                        <div>Type: <span id="details-type">${typeStr}</span></div>
-                        <div>Weight: <span id="details-weight">${data.weight}</span> hectograms</div>
-                        <div>Height: <span id="details-height">${data.height}</span> decimeters</div>
-                    </div>
-                    <div id="details-sub-text">
-                        <h3>Base Attributes</h3>
-                        <div>Health Points: <span id="details-stats-hp">${hp}</span></div>
-                        <div>Attack: <span id="details-stats-atk">${atk}</span></div>
-                        <div>Defense: <span id="details-stats-def">${def}</span></div>
-                    </div>`;
+        let details = document.querySelector("#details");
 
-                    
-                    if(shouldAddToRecent){
-                        let recentList = document.querySelector("#recent-list");
-                        // <div class="recent-list-item">
-                        //     <img src="https://static.pokemonpets.com/images/monsters-images-800-800/147-Dratini.webp" alt="Evolution version image" />
-                        //     <div>Dratini</div>
-                        // </div>
-                        let recentListItem = document.createElement("div");
-                        recentListItem.classList.add("recent-list-item");
-    
-                        let recentListImg = document.createElement("img");
-                        recentListImg.src = data.sprites.front_default;
-                        recentListImg.alt = "Evolution version image";
-    
-                        let nameDiv = document.createElement("div");
-                        nameDiv.textContent = capitalize(data.name);
-    
-                        nameDiv.addEventListener("click", (event)=>{
-                            fetchPokemonDetails(event.target.textContent, false);
-                        });
-    
-                        recentListItem.append(recentListImg, nameDiv);
-    
-                        recentList.append(recentListItem);
-                    }
+        let typeStr = data.types.map((typeEl)=>{
+            return capitalize(typeEl.type.name);
+        }).join("/");
 
-                // let currentPokemonEl = document.querySelector("#current-pokemon");
+        details.innerHTML = `<div id="details-title">
+                <h2>Details</h2>
+            </div>
+            <div id="details-img-container">
+                <img src=${data.sprites.front_default} alt="Image of selected pokémon" />
+            </div>
+            <div id="details-text">
+                <div>Name: <span id="details-name">${capitalize(data.name)}</span></div>
+                <div>Type: <span id="details-type">${typeStr}</span></div>
+                <div>Weight: <span id="details-weight">${Math.round(Number(data.weight)/4.536)}</span> lbs</div>
+                <div>Height: <span id="details-height">${Math.round(Number(data.height)/3.048)}</span> ft</div>
+            </div>
+            <div id="details-sub-text">
+                <h3>Base Attributes</h3>
+                <div>Health Points: <span id="details-stats-hp">${hp}</span></div>
+                <div>Attack: <span id="details-stats-atk">${atk}</span></div>
+                <div>Defense: <span id="details-stats-def">${def}</span></div>
+            </div>`;
 
-                // let img = document.createElement("img");
-                // img.src = data.sprites["front_default"];
-                
-                // currentPokemonEl.append(img);
-            });
+            
+            if(shouldAddToRecent){
+                let recentList = document.querySelector("#recent-list");
+
+                let recentListItem = document.createElement("div");
+                recentListItem.classList.add("recent-list-item");
+
+                let recentListImg = document.createElement("img");
+                recentListImg.src = data.sprites.front_default;
+                recentListImg.alt = "Evolution version image";
+
+                let nameDiv = document.createElement("div");
+                nameDiv.textContent = capitalize(data.name);
+
+                nameDiv.addEventListener("click", (event)=>{
+                    console.log("Trigger");
+                    fetchPokemonDetails(event.target.textContent, false);
+                });
+
+                recentListItem.append(recentListImg, nameDiv);
+
+                recentList.append(recentListItem);
+            }
+            
     } else {
         errMessage.textContent = "Please choose a Pokemon!";
     }
